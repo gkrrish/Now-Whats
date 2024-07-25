d352517e3158aae2cf896baaf64d3b4adf72b7de
Krishna <gkrrish.11@gmail.com>
2024-07-25 16:30:30 +0530

    initial commit

---
+++ src/main/java/com/whats/NowWhatsApplication.java
@@ -0,0 +1,13 @@
+package com.whats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowWhatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowWhatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whats/NowWhatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowWhatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
